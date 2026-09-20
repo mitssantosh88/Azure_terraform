cdd8002bea8e09ca4a90ef3696edda01a71781a5
Santosh19 <santoshy@asdfa.com>
2026-09-21 02:48:26 +0530

    rg name chnage

--- Variable/Backend.tf
+++ Variable/Backend.tf
@@ -1,8 +1,8 @@
-# terraform {
-#   backend "azurerm" {
-#     resource_group_name = "rg-5g-prod"
-#     storage_account_name = "st5gprod"
-#     container_name       = "tfstate"
-#     key                  = "devops/terraform.tfstate"
-#   }
-# }
+terraform {
+  backend "azurerm" {
+    resource_group_name = "rg-prod"
+    storage_account_name = "paartst"
+    container_name       = "tfstate"
+    key                  = "devops/terraform.tfstate"
+  }
+}
