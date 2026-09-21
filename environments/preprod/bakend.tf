b26f5cc91813e2a722808685367de08f738039d4
Santosh19 <santoshy@asdfa.com>
2026-09-22 04:10:02 +0530

    Module add1

--- environments/preprod/bakend.tf
+++ environments/preprod/bakend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-santosh"
-    storage_account_name = "amf5gpreprod"
+    storage_account_name = "amf5gprod"
     container_name       = "tfstate"
     key                  = "preprod.tfstate"
   }
